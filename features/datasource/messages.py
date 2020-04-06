1932c1dc09f53966ce5ef175a90b789d26cd5812
Jéssica Souza <jessica_schelly@hotmail.com>
2020-04-06 17:02:59 -0300

    add datapool

---
+++ features/datasource/messages.py
@@ -0,0 +1,9 @@
+SYSTEM_MESSAGES = {
+    "login_user": [
+        {   
+            "empty_email_field": "O campo email é obrigatório.",
+            "empty_password_field": "O campo senha é obrigatório.",
+            "invalid_credential": "Usuário e/ou senha inválidos.",
+        }
+    ],
+}
---
+++ features/datasource/users.py
@@ -0,0 +1,32 @@
+DATA_ACCESS = {
+	"valid_credential": [
+	    {"username": "amazon.test.jessica@gmail.com",
+     	 "password": "amazontest123",
+         },
+    ],
+
+	"no_credential": [
+	    {"username": "",
+     	 "password": ""}
+    ],
+
+    "invalid_credential": [
+	    {"username": "amazon.test.jessica@gmail.com",
+     	 "password": "amazontest12"}
+    ],
+
+    "invalid_email_format": [
+		{"username": "amazon.test.jessicagmail.com",
+         "password": "amazontest123"}
+	],
+
+    "no_email": [
+		{"username": "",
+         "password": "amazontest123"}
+	],
+
+	"no_password": [
+		{"username": "amazon.test.jessica@gmail.com",
+         "password": ""}
+	],
+}
